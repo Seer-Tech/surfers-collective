120c93c49dfb704ff72321fba1c06e4b14ae4635
harrisono132123 <harrisonjoates@gmail.com>
2024-09-30 10:00:21 +0900

    after interactive bug

--- app/head.tsx
+++ app/head.tsx
@@ -30,13 +30,11 @@ export default function HeadSection() {
 
     <Script
       src="https://code.jquery.com/jquery-3.6.0.min.js"
-      strategy="beforeInteractive"
     />
     <Script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
       crossOrigin="anonymous"
-      strategy="afterInteractive"
     />
 
     <Script id='modalInit' strategy="lazyOnload">
